38b5d1a37878cb53ca5c1f3622d8bc814edece0f
Your Name <gaashaamox1@gmail.com>
2025-02-08 13:05:34 +0500

    added authorisation

---
+++ backend/Controllers/AuthController.js
@@ -0,0 +1,69 @@
+import e from "express";
+import dotenv from "dotenv";
+import User from "../models/User.js";
+import multer from "multer";
+import cloudinary from "cloudinary";
+import bcrypt from "bcrypt";
+
+dotenv.config();
+const router = e.Router();
+
+//waxansamenaynaa multer ka
+const storage = multer.memoryStorage(); //waxan isticmalynaa laptopka storagekisa
+var upload = multer({
+  storage: storage,
+});
+
+const signup = async (req, res) => {
+  try {
+    const { firstName, lastName, userBio, userEmail, userMobile, userName } =
+      req.body;
+
+    //hadii u jiro userka db an ka egno
+    const existingUser = await User.findOne({ userEmail });
+    if (existingUser) {
+      res.status(401).send("User already exists with this email");
+    }
+    //egayna hadii file ka  ina siyen
+    if (!req.file) {
+      return res.status(400).json({
+        error: "No profile image profided",
+      });
+    }
+
+    //cloudinary  aan ku  store gareno fileka io sawirkena
+    const result = await cloudinary.uploader.upload(req.file.path);
+    console.log(result);
+
+    const password = req.body.userPassword;
+    const saltRounds = 10;
+    const salt = await bcrypt.genSalt(saltRounds);
+
+    const encryptedPassword = await bcrypt.hash(password, salt);
+    console.log("Request Body : ", req.body);
+
+    //imika dbka an galino
+    const newUser = newUser({
+      firstName,
+      lastName,
+      userBio,
+      userEmail,
+      userMobile,
+      userName,
+      userPassword: encryptedPassword,
+      profileImage: result.secure_url,
+    });
+    await newUser.save();
+    return res.status(200).json({
+      status: "OK",
+      user: newUser,
+    });
+  } catch (error) {
+    res.status(400).json({
+      error: error.message,
+    });
+    console.log(error);
+  }
+};
+
+export default { signup };
